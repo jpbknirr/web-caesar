fe644175520768ebcaf060422f33907182a36981
John Knirr <jpbknirr@gmail.com>
2017-09-24 17:23:32 -0500

    Got it!
    I got the form.format to work!

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
-      <form method="post">
-            Rotate by:
-            <input type="text" name="rot" value="0"/>
-            <textarea input type="text" name="text"/>
-            </textarea>
-            <input type="submit" value="Submit Query"/>
+        <form method="post">
+            <label for="rot"> Rotate by:</label>
+            <input type="text" name="rot" value="0">
+            <textarea type="text" name="text">{0}</textarea>
+            <br>
+            <input type="submit">
         </form>
     </body>
 </html>
@@ -45,12 +45,11 @@ def encrypt():
     form_text = request.form['text']
     text = str(form_text)
     encrypted_text = rotate_string(text, rot)
-    answer = "<h1>" + encrypted_text + "</h1>"
-    return answer 
+    return form.format(encrypted_text)
 
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 app.run()
